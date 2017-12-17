208eb6eb947a7cb85c674c74d4d0d1af76cef1da
Ed Copeland <ed@Eds-Mac-mini.home>
2017-12-17 01:04:04 +0000

    add options page with contact fields, change file name

---
+++ mu-plugins/custom-posts-n-taxs.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Plugin Name: Woody Plugin
+Description: A plugin to add features to the woody theme
+Version: 1.0
+Author: Ed Copeland
+License: GPLv2
+*/
+
+add_action( 'init', 'create_activity_type' );
+
+function create_activity_type() {
+  register_post_type( 'activites',
+          array(
+              'labels' => array(
+                  'name' => 'Activities',
+                  'singular_name' => 'Activity',
+                  'add_new' => 'Add New',
+                  'add_new_item' => 'Add New Activity',
+                  'edit' => 'Edit',
+                  'edit_item' => 'Edit Activity',
+                  'new_item' => 'New Activity',
+                  'view' => 'View',
+                  'view_item' => 'View Activity',
+                  'search_items' => 'Search Activities',
+                  'not_found' => 'No Activities found',
+                  'not_found_in_trash' => 'No Activities found in Trash',
+                  'parent' => 'Parent Activity'
+              ),
+
+              'public' => true,
+              'menu_position' => 15,
+              'supports' => array( 'title', 'editor', 'thumbnail' ),
+              'taxonomies' => array( '' ),
+              'show_in_rest'      => true,
+              'menu_icon' => plugins_url( 'images/image.png', __FILE__ ),
+              'has_archive' => true
+          )
+      );}
+
+      add_action( 'init', 'create_activity_type_taxonomy', 0 );
+
+      // an Activity Type taxonomy for categorising activities by type
+function create_activity_type_taxonomy() {
+
+
+  $labels = array(
+    'name' => _x( 'Activity Type', 'taxonomy general name' ),
+    'singular_name' => _x( 'Activity Type', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Activity Types' ),
+    'all_items' => __( 'All Activity Types' ),
+    'parent_item' => __( 'Parent Activity Type' ),
+    'parent_item_colon' => __( 'Parent Activity Type:' ),
+    'edit_item' => __( 'Edit Activity Type' ),
+    'update_item' => __( 'Update Activity Type' ),
+    'add_new_item' => __( 'Add New Activity Type' ),
+    'new_item_name' => __( 'New Activity Type Name' ),
+    'menu_name' => __( 'Activity Type' ),
+  );
+
+
+  register_taxonomy('activity_type',array('activites'), array(
+    'hierarchical' => false,
+    'labels' => $labels,
+    'show_ui' => true,
+		'show_in_rest'      => true,
+    'show_admin_column' => true,
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'activity_type' ),
+  ));
+
+}
+
+?>
--- mu-plugins/woody-cmb-fields.php
+++ mu-plugins/woody-cmb-fields.php
@@ -10,7 +10,86 @@
   }
 
 
-  add_action( 'cmb2_admin_init', 'woody_register_theme_options_metabox' );
-  /**
-   * Hook in and register a metabox to handle a theme options page and adds a menu item.
-   */
+add_action( 'cmb2_admin_init', 'woody_register_theme_options_metabox' );
+/**
+* Hook in and register a metabox to handle a theme options page and adds a menu item.
+*/
+function woody_register_theme_options_metabox() {
+ $prefix = '_woody_';
+
+ /**
+  * Registers options page menu item and form.
+  */
+ $cmb_options = new_cmb2_box( array(
+   'id'           => 'woody_theme_options_page',
+   'title'        => esc_html__( 'Woody Contact Details', 'cmb2' ),
+   'object_types' => array( 'options-page' ),
+   'option_key'      => 'woody_contact_details', // The option key and admin menu page slug.
+   'icon_url'        => 'dashicons-palmtree', // Menu icon. Only applicable if 'parent_slug' is left empty.
+
+ ) );
+
+
+  $cmb_options->add_field( array(
+    'name'       => esc_html__( 'Company Name', 'cmb2' ),
+    'id'         => $prefix . 'company_name',
+    'type'       => 'text',
+
+  ) );
+
+  $cmb_options->add_field( array(
+    'name'       => esc_html__( 'C/O', 'cmb2' ),
+    'id'         => $prefix . 'c_o',
+    'type'       => 'text',
+
+  ) );
+
+  $cmb_options->add_field( array(
+    'name'       => esc_html__( 'Address', 'cmb2' ),
+    'id'         => $prefix . 'address',
+    'type'       => 'text',
+    'repeatable'      => true,
+  ) );
+
+  $cmb_options->add_field( array(
+    'name'       => esc_html__( 'Town/City', 'cmb2' ),
+    'id'         => $prefix . 'town',
+    'type'       => 'text',
+
+  ) );
+
+  $cmb_options->add_field( array(
+    'name'       => esc_html__( 'County', 'cmb2' ),
+    'id'         => $prefix . 'county',
+    'type'       => 'text',
+
+  ) );
+
+  $cmb_options->add_field( array(
+    'name'       => esc_html__( 'Postcode', 'cmb2' ),
+    'id'         => $prefix . 'postcode',
+    'type'       => 'text',
+
+  ) );
+
+  $cmb_options->add_field( array(
+    'name'       => esc_html__( 'Telephone', 'cmb2' ),
+    'id'         => $prefix . 'Tel',
+    'type'       => 'text',
+
+  ) );
+
+ $cmb_options->add_field( array(
+   'name' => esc_html__( 'Twitter URL', 'cmb2' ),
+   'id'   => $prefix . 'url',
+   'type' => 'text_url',
+
+ ) );
+
+ $cmb_options->add_field( array(
+   'name' => esc_html__( 'Email', 'cmb2' ),
+   'id'   => $prefix . 'email',
+   'type' => 'text_email',
+ ) );
+
+}
