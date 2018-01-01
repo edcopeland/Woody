57cce38e1db0136042a1e8914a75b60299b6a0b6
Ed Copeland <ed_copeland@hotmail.co.uk>
2018-01-01 00:39:54 +0000

    simplify activities page, order activities by activity type

--- themes/woody/src/page-activities.php
+++ themes/woody/src/page-activities.php
@@ -30,61 +30,38 @@
 						</section>
 							<!--<footer class="article-footer">
 
-            <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
+						<?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 							</footer> -->
 						</article>
 
-	<?php endwhile;?>
-       <?php
-		      $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-		      $mypost = array( 'post_type' => 'activities','paged' => $paged,  'posts_per_page' => -1,'category__not_in' => 14);
-		      $loop = new WP_Query( $mypost )
-        ?>
-  		<?php if (have_posts()) :
-				while ( $loop->have_posts() ) : $loop->the_post();?>
-							<article  id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-                <div class="page-post">
-									<header  id = "post-head" class="article-header">
-                    <?php
-                        if ( has_post_thumbnail() ) {
-                            the_post_thumbnail('all-post-img',array( 'id' => 'all-posts-thumb'));
-                        }
-                        ?>
-                  </header>
+						<?php endwhile;?>
 
-									<section id="section" class="entry-content cf" itemprop="articleBody">
-                    <h2  class="page-title"> <a href="<?php echo get_permalink(); ?>"> <?php the_title(); ?>  </a> </h2>
-                    	<?php  the_excerpt(); ?>
-                     	<?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-                    <a id="more-link" href="<?php echo get_permalink(); ?>"> Read More...</a>
-									</section>
-                                                              <!-- Start date and end date-->
+						<?php $cats = get_terms('activity_type');
+						foreach ($cats as $cat){
+							// Make a header for the cateogry
+							echo "<h2>".$cat->name."</h2>";
+								$cat_id= $cat->term_id;
+								$args=array(
+									'posts_per_page' => -1,
+									'post_type' => 'activities',
+									'tax_query' => array(
+			 						array(
+									 'taxonomy' => 'activity_type',
+									 'terms' => $cat_id,
+								 ))
+								);
+							 $loop = new WP_Query( $args );
+							  if (have_posts()) :
+							  while ( $loop->have_posts() ) : $loop->the_post();
+								?>
+								<h2  class="page-title"> <a href="<?php echo get_permalink(); ?>"> <?php the_title(); ?>  </a> </h2>
+									<?php
 
-                  <footer class="article-footer">
+								endwhile;
+							endif;
+}?>
 
-                  </footer>
 
-	              </div>
-							</article>
-					<?php
-				endwhile; ?>
-			<?php //bones_page_navi('','',$loop); ?>
-
-			<?php
-			else : ?>
-			<article id="post-not-found" class="hentry cf">
-					<header class="article-header">
-						<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-	                                                                          </header>
-					<section class="entry-content">
-						<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-          </section>
-          <footer class="article-footer">
-						<p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
-          </footer>
-			</article>
-			<?php
-			endif; ?>
 			</div>
 			<?php //get_sidebar(); ?>
 		</div>
