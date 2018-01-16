c4c144d66eefde3a4f5d2839aba700b5f19dcede
Ed Copeland <ed_copeland@hotmail.co.uk>
2018-01-16 19:12:59 +0000

    remove archive label fromarchive title  on archive page

--- themes/woody/src/functions.php
+++ themes/woody/src/functions.php
@@ -276,6 +276,23 @@ $args = array(
 
 add_theme_support( 'custom-header',$args );
 
+function my_theme_archive_title( $title ) {
+    if ( is_category() ) {
+        $title = single_cat_title( '', false );
+    } elseif ( is_tag() ) {
+        $title = single_tag_title( '', false );
+    } elseif ( is_author() ) {
+        $title = '<span class="vcard">' . get_the_author() . '</span>';
+    } elseif ( is_post_type_archive() ) {
+        $title = post_type_archive_title( '', false );
+    } elseif ( is_tax() ) {
+        $title = single_term_title( '', false );
+    }
+
+    return $title;
+}
+
+add_filter( 'get_the_archive_title', 'my_theme_archive_title' );
 
 
 /* DON'T DELETE THIS CLOSING TAG */ ?>
