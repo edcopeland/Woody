0e5712a61a367dd6f22acafd9d9698baa084abe1
Ed Copeland <ed_copeland@hotmail.co.uk>
2018-02-10 22:55:38 +0000

    make a header bar div to join logo and nav, position absolute, nav bar styles

--- themes/woody/src/header.php
+++ themes/woody/src/header.php
@@ -47,35 +47,38 @@
 		<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
 				<div id="inner-header" class="wrap cf">
-					<?php if ( function_exists( 'the_custom_logo' ) ) {
-    				the_custom_logo();
-						} ?>
-					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					 <!-- <p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization">
-						<a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a>
-					 </p> -->
+					<div class="header-bar">
+						<?php if ( function_exists( 'the_custom_logo' ) ) {
+	    				the_custom_logo();
+							} ?>
+						<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
+						 <!-- <p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization">
+							<a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a>
+						 </p> -->
 
-					<?php // if you'd like to use the site description you can un-comment it below ?>
-					<?php // bloginfo('description'); ?>
+						<?php // if you'd like to use the site description you can un-comment it below ?>
+						<?php // bloginfo('description'); ?>
 
-					<div id = "nav-container" class ="nav-container">
-						<nav id= "main-nav" class= "main-nav" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-							<a class="toggleMenu toggle-white" href="#">  &#9776; </a>
-							<?php wp_nav_menu(array(
-	    					         'container' => false,                           // remove nav container
-	    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-	    					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-	    					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
-	    					         'theme_location' => 'main-nav',                 // where it's located in the theme
-	    					         'before' => '',                                 // before the menu
-	        			               'after' => '',                                  // after the menu
-	        			               'link_before' => '',                            // before each link
-	        			               'link_after' => '',                             // after each link
-	        			               'depth' => 0,                                   // limit the depth of the nav
-	    					         'fallback_cb' => ''                             // fallback function (if there is one)
-							)); ?>
+						<div id = "nav-container" class ="nav-container">
+							<nav id= "main-nav" class= "main-nav" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+								<a class="toggleMenu toggle-white" href="#">  &#9776; </a>
+								<?php wp_nav_menu(array(
+		    					         'container' => false,                           // remove nav container
+		    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+		    					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+		    					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
+		    					         'theme_location' => 'main-nav',                 // where it's located in the theme
+		    					         'before' => '',                                 // before the menu
+	    			               'after' => '',                                  // after the menu
+	    			               'link_before' => '',                            // before each link
+	    			               'link_after' => '',                             // after each link
+	    			               'depth' => 0,                                   // limit the depth of the nav
+		    					         'fallback_cb' => ''                             // fallback function (if there is one)
+								)); ?>
 
-						</nav>
+							</nav>
+					</div>
+
 					</div><!-- navcontainer -->
 
 					<div class="header-image">
