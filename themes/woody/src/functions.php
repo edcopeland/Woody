bb1deea667fed48d52b8b163180462730ea3e51d
Ed Copeland <ed_copeland@hotmail.co.uk>
2018-03-20 22:04:13 +0000

    add password protect to front of site

--- themes/woody/src/functions.php
+++ themes/woody/src/functions.php
@@ -297,5 +297,12 @@ function theme_archive_title( $title ) {
 
 add_filter( 'get_the_archive_title', 'theme_archive_title' );
 
+function protect_whole_site() {
+    if ( !is_user_logged_in() ) {
+        auth_redirect();
+    }
+}
+add_action ('template_redirect', 'protect_whole_site');
+
 
 /* DON'T DELETE THIS CLOSING TAG */ ?>
